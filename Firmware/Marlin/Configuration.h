4051b0ef51eeeda822b386c1ec4d80c581211564
StrikeEagleCC <35362359+StrikeEagleCC@users.noreply.github.com>
2019-03-21 02:11:04 -0500

    Tune movement settings
    Increase extruder max feedrate and acceleration and jerk to facilitate higher linear advance settings and faster filament unloads.
    
    Increased decreased x/y jerk and increased x/y acceleration.

--- Firmware/Marlin/Configuration.h
+++ Firmware/Marlin/Configuration.h
@@ -624,7 +624,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 20, 40 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 20, 100 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -632,7 +632,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 10000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -642,7 +642,7 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION           500    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_ACCELERATION           600    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
 
@@ -654,10 +654,10 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                10.0
-#define DEFAULT_YJERK                10.0
+#define DEFAULT_XJERK                 5.0
+#define DEFAULT_YJERK                 5.0
 #define DEFAULT_ZJERK                 0.4
-#define DEFAULT_EJERK                13.0
+#define DEFAULT_EJERK                25.0
 
 /**
  * S-Curve Acceleration
