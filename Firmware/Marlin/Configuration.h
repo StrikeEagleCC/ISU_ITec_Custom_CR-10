c1bd5ef961ae1d8c7724051050b4f89efde005f6
StrikeEagleCC <35362359+StrikeEagleCC@users.noreply.github.com>
2019-03-22 19:56:07 -0500

    Update Configuration.h
    Bring extruder acceleration and jerk back down, as the high values led to missed steps and under extrusion when printing thick layers at high speed.

--- Firmware/Marlin/Configuration.h
+++ Firmware/Marlin/Configuration.h
@@ -632,7 +632,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 5000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -657,7 +657,7 @@
 #define DEFAULT_XJERK                 5.0
 #define DEFAULT_YJERK                 5.0
 #define DEFAULT_ZJERK                 0.4
-#define DEFAULT_EJERK                25.0
+#define DEFAULT_EJERK                15.0
 
 /**
  * S-Curve Acceleration
